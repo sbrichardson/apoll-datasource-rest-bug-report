6d44817e78613821f763bc4388717c1e90a037a2
sbrichardson <srichardson@reactual.io>
2018-08-10 16:43:06 +0800

    fix project, update apollo-datasource, install express, apollo-server-express, fix dataSources on Apollo server instantiation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server';
-import { RESTDataSource } from 'apollo-datasource-rest';
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { RESTDataSource } from 'apollo-datasource-rest'
 
 class RestApi extends RESTDataSource {
   constructor() {
-    super();
-    this.baseURL = `http://localhost:8000/api`;
+    super()
+    this.baseURL = `http://localhost:8000/api`
   }
 
   async getDogs() {
-    return this.get('dogs');
+    return this.get('dogs')
   }
 }
 
@@ -22,26 +22,26 @@ const typeDefs = gql`
   type Query {
     dogs: [Dog]
   }
-`;
+`
 
 const resolvers = {
   Query: {
-    dogs: (_, args, { restApi }) => restApi.getDogs()
+    dogs: (_, args, { dataSources: { restApi } }) => restApi.getDogs()
   }
-};
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => ({
+  dataSources: () => ({
     restApi: new RestApi()
   })
-});
+})
 
-const app = express();
+const app = express()
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app })
 
 app.listen({ port: 4000 }, () => {
-  console.log(`🚀  Server ready at ${4000}`);
-});
+  console.log(`🚀  Server ready at ${4000}`)
+})
